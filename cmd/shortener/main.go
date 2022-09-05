ba67d15ee501be44da15c4030df3d14e0b16609a
paukshto <dr.tvn@yandex.ru>
2022-09-06 00:39:32 +0300

    increment 1 added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/T-V-N/gourlshortener/internal/handler"
+	"github.com/T-V-N/gourlshortener/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+	handler := handler.InitHandler(st)
+	http.HandleFunc("/", handler.HandleRequest)
+	fmt.Println("go!")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ internal/handler/handler.go
@@ -0,0 +1,57 @@
+package handler
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/T-V-N/gourlshortener/internal/storage"
+)
+
+type Handler struct {
+	storage *storage.Storage
+}
+
+type URL struct {
+    URL string `json:"URL"`
+}
+
+func InitHandler(st *storage.Storage) *Handler {
+	return &Handler{storage: st}
+}
+
+func (h *Handler) HandleRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	case http.MethodGet: 
+		id := strings.Split(r.URL.Path, "/")[1]
+		if id == "" {
+			http.Error(w, "no short URL provided", 500)
+			return
+		}
+
+		url, err := h.storage.GetUrl(id) 
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		w.Header().Add("Location", url)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+	case http.MethodPost: 
+		url, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		hash := h.storage.SaveUrl(string(url))
+		resp, err := json.Marshal(URL{r.Host+"/"+hash})
+		w.WriteHeader(http.StatusCreated)
+		w.Write(resp)
+
+	default: 
+		http.Error(w, "Wrong request", http.StatusBadRequest)
+		return
+	}
+}
