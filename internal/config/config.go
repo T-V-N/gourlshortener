87b63777793fee1bd6a3dda04431ac9d46a728ea
paukshto <dr.tvn@yandex.ru>
2022-10-09 18:48:04 +0300

    db default path fixed

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 type Config struct {
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"../../file_storage/db"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 func Init() (*Config, error) {
