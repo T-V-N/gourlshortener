864ace2896743d5b4ba62c958ae051c3b61c2e89
paukshto <dr.tvn@yandex.ru>
2023-02-07 15:05:54 +0300

    add example

---
+++ internal/handler/example_handler_test.go
@@ -0,0 +1,31 @@
+package handler_test
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/T-V-N/gourlshortener/internal/app"
+	"github.com/T-V-N/gourlshortener/internal/handler"
+	"github.com/T-V-N/gourlshortener/internal/storage"
+)
+
+func Example() {
+	link := "https://yandex.ru"
+
+	fmt.Print(link)
+
+	cfg, _ := InitTestConfig()
+	st := storage.InitStorage(map[string]storage.URL{}, cfg)
+	a := app.InitApp(st, cfg)
+	hn := handler.InitHandler(a)
+
+	request := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer([]byte(link)))
+	w := httptest.NewRecorder()
+	hn.HandlePostURL(w, request)
+
+	resBody := w.Body.Bytes()
+
+	fmt.Print(resBody)
+}
