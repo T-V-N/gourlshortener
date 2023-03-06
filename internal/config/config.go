945cbda284758a4a7c9d82df4e4f86118afe9f52
paukshto <dr.tvn@yandex.ru>
2023-03-06 21:26:00 +0300

    iter21

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,27 +2,42 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/caarlos0/env/v6"
 )
 
 // Config for the service
 type Config struct {
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"` // URL where server will be started
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`           // Server port
-	FileStoragePath string `env:"FILE_STORAGE_PATH"`                           // Path to a file which will be used as a storage
-	SecretKey       string `env:"SECRET_KEY" envDefault:"hello"`               // Secret for hashing ops
-	DatabaseDSN     string `env:"DATABASE_DSN"`                                // Database connection string for DB-style storage
-	EnableHTTPS     bool   `env:"ENABLE_HTTPS" envDefault:"false"`             // flag enables https
+	BaseURL         string `env:"BASE_URL" json:"base_url"`                   // URL where server will be started
+	ServerAddress   string `env:"SERVER_ADDRESS" json:"server_address"`       // Server port
+	FileStoragePath string `env:"FILE_STORAGE_PATH" json:"file_storage_path"` // Path to a file which will be used as a storage
+	SecretKey       string `env:"SECRET_KEY" envDefault:"hello"`              // Secret for hashing ops
+	DatabaseDSN     string `env:"DATABASE_DSN" json:"database_dsn"`           // Database connection string for DB-style storage
+	EnableHTTPS     bool   `env:"ENABLE_HTTPS" json:"enable_https"`           // flag enables https                                                   // Config json path
 }
 
 // Init tries to parse os.env and flags passed to the service run command.
 // Flags have priority over os envs.
 // Then in returns a config ready for usage by other service layers.
 func Init() (*Config, error) {
-	cfg := &Config{}
+	cfg := &Config{BaseURL: "http://localhost:8080", ServerAddress: ":8080", EnableHTTPS: false}
+
+	cfgPath := os.Getenv("CONFIG")
+	flag.StringVar(&cfgPath, "c", cfgPath, "Config file")
+	flag.Parse()
+
+	if cfgPath != "" {
+		fileCfg, err := parseFileConfig(cfgPath)
+
+		if err == nil {
+			cfg = fileCfg
+		}
+	}
+
 	err := env.Parse(cfg)
 
 	if err != nil {
@@ -39,4 +54,21 @@ func Init() (*Config, error) {
 	flag.Parse()
 
 	return cfg, nil
+}
+
+func parseFileConfig(name string) (*Config, error) {
+	file, err := os.Open(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fileCfg := &Config{}
+	err = json.NewDecoder(file).Decode(&fileCfg)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fileCfg, nil
 }
